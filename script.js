a8511920d0c03cd6419725342a42f23b69d9ef12
EricOrrDev <155666503+EricOrrDev@users.noreply.github.com>
2024-12-11 00:18:28 -0800

    inital commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('contactButton').addEventListener('click', function() {
+    const contactForm = document.getElementById('contactForm');
+    contactForm.classList.toggle('active');
+});
